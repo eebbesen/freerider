bd3b2a479d40f4e776feba18900680c08542e3df
Nathan Dintenfass <nathan@dintenfass.com>
2015-07-25 00:42:25 -0700

    Change the spec for the Geolocutor to use a different service to avoid google api limits when doing CI. As a result the API returns slightly different numbers, so round some of the tests to lower granularity to allow for this while still doing the right reality check. Arguably, this should not be testing a 3rd party gems ability to talk to external services and should just stub that out, but that is another matter altogether ;)

--- spec/geolocutor_spec.rb
+++ spec/geolocutor_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
+Geocoder.configure(
+  lookup: :nominatim,
+  http_headers: { "User-Agent" => "freerider gem tests (github.com/eebbesen/freerider)" }
+)
+
 describe Geolocutor do
   let(:vehicle) { '{"address":"Grand Ave 1600, 55105 St Paul","coordinates":[-93.16789,44.93999,0],"engineType":"CE","exterior":"GOOD","fuel":26,"interior":"GOOD","name":"AB0000","smartPhoneRequired":false,"vin":"AAAAA0AA0AA000000"}' }
   let(:vehicle_2) { '{"address":"10th & Park   park spot","coordinates":[-93.26529,44.97035,0],"engineType":"CE","exterior":"GOOD","fuel":18,"interior":"GOOD","name":"AB1111","smartPhoneRequired":false,"vin":"BAAAA0AA0AA000000"}' }
@@ -10,12 +15,12 @@ describe Geolocutor do
     it 'should create coordinates from address' do
       geolocutor = Geolocutor.new('1600 Grand Ave, Saint Paul, MN')
 
-      expect(geolocutor.coordinates).to eq([44.9397631, -93.1687294])
+      expect(geolocutor.coordinates.map{|i| i.round(2)}).to eq([44.94, -93.17])
     end
 
     it 'should preserve passed-in coordinates' do
       geolocutor = Geolocutor.new [44.93999, -93.16789]
-      expect(geolocutor.coordinates).to eq([44.9397631, -93.1687294])
+      expect(geolocutor.coordinates.map{|i| i.round(2)}).to eq([44.94, -93.17])
     end
 
     it 'should raise an error if no location passed in' do
@@ -27,7 +32,7 @@ describe Geolocutor do
     it 'should grab coordinates from vehicle JSON' do
       geolocutor = Geolocutor.new('1600 Grand Ave, Saint Paul, MN')
       coordinates = geolocutor.send(:extract_coordinates, JSON.parse(vehicle))
-      expect(coordinates).to eq([44.93999, -93.16789])
+      expect(coordinates.map{|i| i.round(2)}).to eq([44.94, -93.17])
     end
   end
 
@@ -35,20 +40,20 @@ describe Geolocutor do
     before do
       @geolocutor = Geolocutor.new('1600 Grand Ave, Saint Paul, MN')
       # avoid rate limiting errors
-      puts 'Avoid rate limiting errors on Geolocutor by sleeping 3 seconds...'
-      sleep 3
+      puts 'Avoid rate limiting errors on Geolocutor by sleeping...'
+      sleep 1
       puts '...done sleeping'
     end
 
     it 'should return zero when the from and to are the same' do
       distance = @geolocutor.distance_from_here(JSON.parse(vehicle))
-      expect(distance).to eq 0
+      expect(distance.round).to eq 0
     end
 
     context 'when looking for the disance in miles' do
       it 'should return the proper value' do
         distance = @geolocutor.distance_from_here(JSON.parse(vehicle_2))
-        expect(distance).to eq 3.215852550959139
+        expect(distance.round).to eq 3
       end
     end
 
@@ -56,7 +61,7 @@ describe Geolocutor do
       it 'should return the proper value' do
         geolocutor = Geolocutor.new('1600 Grand Ave, Saint Paul, MN', true)
         distance = geolocutor.distance_from_here(JSON.parse(vehicle_2))
-        expect(distance).to eq 8.329019877940437
+        expect(distance.round).to eq 8
       end
     end
   end
